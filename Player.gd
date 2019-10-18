b0865a9dadeda18d7c81b1368c7b12f2ec2cde3e
tomon9086 <tomo.mouse.9086@gmail.com>
2019-10-18 13:32:35 +0900

    :recycle:revert changes

--- Player.gd
+++ Player.gd
@@ -1,35 +1,30 @@
 extends Area2D
 
-func _ready():
-	pass
+const speed = 1
 
-const speed = 7
-
-func _unhandled_input(event):
-	if not event is InputEventKey:
-		return
-
+func move():
 	var velocity = Vector2()
-
-	if event.pressed:
-		if event.scancode == KEY_RIGHT:
-			velocity.x += 1
-			$AnimationPlayer.set_current_animation("walk_right")
-		if event.scancode == KEY_LEFT:
-			velocity.x -= 1
-			$AnimationPlayer.set_current_animation("walk_left")
-		if event.scancode == KEY_DOWN:
-			velocity.y += 1
-			$AnimationPlayer.set_current_animation("walk_down")
-		if event.scancode == KEY_UP:
-			velocity.y -= 1
-			$AnimationPlayer.set_current_animation("walk_up")
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		$AnimationPlayer.set_current_animation("walk_right")
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		$AnimationPlayer.set_current_animation("walk_left")
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		$AnimationPlayer.set_current_animation("walk_down")
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		$AnimationPlayer.set_current_animation("walk_up")
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
 	else:
 		$AnimationPlayer.set_current_animation("")
 
-	if velocity.length() > 0:
-		self.position += velocity.normalized() * speed
+	self.position += velocity
 
+func _ready():
+	pass
 
 func _process(delta):
-	pass
+	move()
